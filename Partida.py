44acc74c8ebecfb44ddabf81771d8a3eb8128978
alexandrejuk <ale_santos.soares@hotmail.com>
2018-11-08 09:26:57 -0200

    match: improve to match win

--- Partida.py
+++ Partida.py
@@ -1,60 +1,72 @@
 import random
+import algoritmoWin
 
 class Partida:
-  def __init__(self, selecaoA, selecaoB, tipoPartida):
+  def __init__(self, selecaoA, selecaoB):
     self.__selecaoA = selecaoA
     self.__statusSelecaoA = []
+
     self.__selecaoB = selecaoB
     self.__statusSelecaoB = []
+
     self.__vencedor = None
     self.__perdedor = None
+
     self.__golsSelecaoA = 0
     self.__golsSelecaoB = 0
-    self.__tipoPartida = tipoPartida
 
-  def __calcularPartida(self):
-    return round((round(random.random(),2) * 0.87 ), 2)
 
-  def criarPartida(self):
-    for i in range(1,20):
-      self.__statusSelecaoA.append(self.__calcularPartida())
-      self.__statusSelecaoB.append(self.__calcularPartida())
+  def iniciarPartida(self):
+
+    for i in range(1,91):
+      self.__statusSelecaoA.append(algoritmoWin.calcWin(random.random, self.__selecaoA.getAtk()))
+      self.__statusSelecaoB.append(algoritmoWin.calcWin(random.random, self.__selecaoB.getAtk()))
+
+    self.__calcularGols()
+    
+    self.__selecaoA.putGols(self.__golsSelecaoA)
+    self.__selecaoB.putGols(self.__golsSelecaoB)
+
+    if self.__golsSelecaoA == self.__golsSelecaoB: 
+      
+      if self.__selecaoA.getGols() > self.__selecaoB.getGols():
+        self.__vencedor = self.__selecaoA.getNome()
+        self.__perdedor = self.__selecaoB.getNome()
+        self.__selecaoA.putVitoria(1)
+
+      else:
+        self.__vencedor = self.__selecaoB.getNome()
+        self.__perdedor = self.__selecaoA.getNome()
+        self.__selecaoB.putVitoria(1)
+
+    elif self.__golsSelecaoA > self.__golsSelecaoB:
+      self.__vencedor = self.__selecaoA.getNome()
+      self.__perdedor = self.__selecaoB.getNome()
+      self.__selecaoA.putVitoria(1)
+
+    else:
+      self.__vencedor = self.__selecaoB.getNome()
+      self.__perdedor = self.__selecaoA.getNome()
+      self.__selecaoB.putVitoria(1)
   
+
+  def __calcularGols(self):
+
+    for i in range(len(self.__statusSelecaoA)):
+
+      if self.__statusSelecaoA[i] > self.__statusSelecaoB[i]:
+        self.__golsSelecaoA += self.__checarGol(self.__statusSelecaoA[i])
+
+      elif self.__statusSelecaoA[i] < self.__statusSelecaoB[i]:
+        self.__golsSelecaoB += self.__checarGol(self.__statusSelecaoB[i])
+    
   def __checarGol(self, a):
     if a > 0.84 and a <= 1:
       return 1
     return 0
 
-  def definirGols(self):
-    for i in range(len(self.__statusSelecaoA)):
-      if self.__statusSelecaoA[i] > self.__statusSelecaoB[i]:
-        self.__registrarGolsMarcadosA(self.__checarGol(self.__statusSelecaoA[i]))
-      elif self.__statusSelecaoA[i] < self.__statusSelecaoB[i]:
-        self.__registrarGolsMarcadosB(self.__checarGol(self.__statusSelecaoB[i]))
+  def vencedor(self):
+    return self.__vencedor 
 
-  def criarPenaltis(self):
-    return 'partida criar'
-
-  def __registrarGolsMarcadosA(self, gols):
-    self.__golsSelecaoA += gols
-    
-  def __registrarGolsMarcadosB(self, gols):
-    self.__golsSelecaoB += gols
-
-  def registrarVencedor(self, selecao):
-    self.__vencedor = selecao
-
-  def registrarPerdedor(self, selecao):
-    self.__perdedor = selecao
-
-  def informarVencedor(self):
-    return self.__vencedor
-  
-  def informarPerdedor(self):
+  def perdedor(self):
     return self.__perdedor
-
-  def informarGolsSelecaoA(self):
-    return self.__golsSelecaoA
-  
-  def informarGolsSelecaoB(self):
-    return self.__golsSelecaoB
