b9bc8871c912597568ddabb4abc68403ba63e423
alexandrejuk <ale_santos.soares@hotmail.com>
2018-11-08 09:37:17 -0200

    chore: modify some files

--- main.py
+++ main.py
@@ -1,48 +1,49 @@
-# import random
-# import selecoesData
-# from Selecao import Selecao
-# from Torneio import Torneio
+import random
+import selecoesData
 
-# selecoes = selecoesData.selecoes()
-# qtdSelecoes = len(selecoes)
-# torneio = Torneio()
-
-# count = 0
-# grupo = []
-
-# for i in range(8):
-#   letraGrupo = chr(65 + count) 
-#   selecao = Selecao(selecoes[random.randrange(0, qtdSelecoes)], 0, 0, "grupo " + letraGrupo)
-
-#   if len(grupo) == 0:
-#     grupo.append(selecao)
-
-#   elif len(grupo) < 4:
-#     grupo.append(selecao)
-
-#   elif len(grupo) == 4:
-#     torneio.registrarGroupo(grupo)
-
-#     grupo = []
-#     count += 1
-
-# iSel = torneio.informarGroupos()
-# for i in range(len(iSel)):
-#  for j in range(4):
-#    print(iSel[i][j].getFase())
-
-from Partida import Partida
 from Selecao import Selecao
+from Torneio import Torneio
+from Partida import Partida
 
-selecaoA = Selecao("Brasil", 0, 0, "grupo A", 0.84)
-selecaoB = Selecao("Argentina", 0, 0, "grupo A", 0.85)
-grupo = [selecaoA, selecaoB]
+selecoes = selecoesData.selecoes()
+qtdSelecoes = len(selecoes)
+torneio = Torneio()
 
-partida = Partida(grupo[0], grupo[1])
-iniciar = partida.iniciarPartida()
+count = 0
+grupo = []
 
-v = str(selecaoA.getGols())
-p = str(selecaoB.getGols())
+for i in range(40):
+  letraGrupo = chr(65 + count) 
+  selecao = Selecao(selecoes[random.randrange(0, qtdSelecoes)], 0, 0, "grupo " + letraGrupo, 0.84)
 
-print('Gols A ==============>>> ', selecaoA.getNome(), v)
-print('Gols B ==============>>> ', selecaoB.getNome(), p)
+  if len(grupo) == 0:
+    grupo.append(selecao)
+
+  elif len(grupo) < 4:
+    grupo.append(selecao)
+
+  elif len(grupo) == 4:
+    torneio.registrarGroupo(grupo)
+
+    grupo = []
+    count += 1
+
+iSel = torneio.informarGroupos()
+for i in range(len(iSel)):
+ for j in range(4):
+   print(iSel[i][j].getFase() +': '+iSel[i][j].getNome())
+
+
+
+# selecaoA = Selecao("Brasil", 0, 0, "grupo A", 0.84)
+# selecaoB = Selecao("Argentina", 0, 0, "grupo A", 0.85)
+# grupo = [selecaoA, selecaoB]
+
+# partida = Partida(grupo[0], grupo[1])
+# iniciar = partida.iniciarPartida()
+
+# v = str(selecaoA.getGols())
+# p = str(selecaoB.getGols())
+
+# print('Gols A ==============>>> ', selecaoA.getNome(), v)
+# print('Gols B ==============>>> ', selecaoB.getNome(), p)
